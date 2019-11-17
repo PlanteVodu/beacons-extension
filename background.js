c6f7a7726d6dd5b8211ef1028e4e96b98e28c73f
FildasPA <fildas.pa3@gmail.com>
2019-11-17 19:08:54 +0100

    Check if url is bookmarked inside Beacons
    Check if a URL is bookmarked inside Beacons by requesting the server.
    Update the menu icon accordingly.

--- background.js
+++ background.js
@@ -1,5 +1,5 @@
 var currentTab;
-var currentBookmark;
+var bookmarkExists;
 
 /*
  * Updates the browserAction icon to reflect whether the current page
@@ -7,18 +7,18 @@ var currentBookmark;
  */
 function updateIcon() {
   browser.browserAction.setIcon({
-    path: currentBookmark ? {
-      19: "icons/star-filled-19.png",
-      38: "icons/star-filled-38.png"
+    path: bookmarkExists ? {
+      24: "icons/icon-beacon.png",
     } : {
-      19: "icons/star-empty-19.png",
-      38: "icons/star-empty-38.png"
+      24: "icons/icon-empty.png",
     },
     tabId: currentTab.id
   });
   browser.browserAction.setTitle({
     // Screen readers can see the title
-    title: currentBookmark ? 'Unbookmark it!' : 'Bookmark it!',
+    title: bookmarkExists ?
+      'Remove this page from Beacons.' :
+      'Add this page to Beacons.',
     tabId: currentTab.id
   });
 }
@@ -27,17 +27,13 @@ function updateIcon() {
  * Add or remove the bookmark on the current page.
  */
 function toggleBookmark() {
-  if (currentBookmark) {
-    browser.bookmarks.remove(currentBookmark.id);
-  } else {
-    browser.bookmarks.create({title: currentTab.title, url: currentTab.url});
-  }
+  console.warn("TODO: toggleBookmark()");
 }
 
 browser.browserAction.onClicked.addListener(toggleBookmark);
 
 /*
- * Switches currentTab and currentBookmark to reflect the currently active tab
+ * Switches currentTab and bookmarkExists to reflect the currently active tab
  */
 function updateActiveTab(tabs) {
 
@@ -48,15 +44,21 @@ function updateActiveTab(tabs) {
     return supportedProtocols.indexOf(url.protocol) != -1;
   }
 
+  function searchBookmark() {
+    const url = `http://localhost:5000/bmexists?url=${currentTab.url}`;
+    fetch(url).then(function(response) {
+      response.json().then(function(exists) {
+        bookmarkExists = JSON.parse(exists);
+        updateIcon();
+      });
+    });
+  }
+
   function updateTab(tabs) {
     if (tabs[0]) {
       currentTab = tabs[0];
       if (isSupportedProtocol(currentTab.url)) {
-        var searching = browser.bookmarks.search({url: currentTab.url});
-        searching.then((bookmarks) => {
-          currentBookmark = bookmarks[0];
-          updateIcon();
-        });
+        searchBookmark();
       } else {
         console.log(`Bookmark it! does not support the '${currentTab.url}' URL.`)
       }
@@ -66,12 +68,6 @@ function updateActiveTab(tabs) {
   var gettingActiveTab = browser.tabs.query({active: true, currentWindow: true});
   gettingActiveTab.then(updateTab);
 }
-
-// listen for bookmarks being created
-browser.bookmarks.onCreated.addListener(updateActiveTab);
-
-// listen for bookmarks being removed
-browser.bookmarks.onRemoved.addListener(updateActiveTab);
 
 // listen to tab URL changes
 browser.tabs.onUpdated.addListener(updateActiveTab);
